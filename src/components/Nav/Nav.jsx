ddb0ac07fa8d7bb4eaf6ba32a4644d6b49465d65
Bruno Chaves <brunochavesuk@icloud.com>
2025-03-20 16:58:56 -0300

    Iniciando a Navbar

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,13 @@
+import * as S from './styles';
+
+function Nav() {
+  return (
+    <S.NavContainer>
+   <h1>Olá Mundo!</h1>
+  
+  </S.NavContainer>
+  )
+  
+}
+
+export default Nav;
---
+++ src/components/Nav/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const NavContainer = styled.div`
+  font-family: 'Poppins', sans-serif;
+
+  h1 {
+    color: #0095E5;
+    font-size: 2rem;
+    text-align: center;
+    margin-top: 20px;
+  }
+`;
