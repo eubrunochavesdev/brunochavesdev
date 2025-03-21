1fb480a754d775a962ba67860aa9561db7434752
Bruno Chaves <brunochavesuk@icloud.com>
2025-03-20 21:56:22 -0300

    Adiciona estilos responsivos e ajustes na navegação

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,26 +1,33 @@
-import * as S from './styles';
-import logo from '../../assets/brunodev.svg';
+import { useState } from "react";
+import * as S from "./styles";
+import logo from "../../assets/brunodev.svg";
 
 function Nav() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <S.NavContainer>
-   
-   <a href="#inicio">
+      <a href="#inicio">
         <img src={logo} alt="Logo Bruno Dev" />
       </a>
-   
-   <nav>
+
+      {/* Botão do Menu Hamburguer */}
+      <S.MenuButton onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </S.MenuButton>
+
+      {/* Menu Responsivo */}
+      <S.NavMenu open={menuOpen}>
         <ul>
-          <li><a href="#inicio">Início</a></li>
-          <li><a href="#sobre">Sobre</a></li>
-          <li><a href="#skills">Skills</a></li>
-          <li><a href="#projetos">Projetos</a></li>
-          <li><a href="#contato">Contato</a></li>
+          <li><a href="#inicio" onClick={() => setMenuOpen(false)}>Início</a></li>
+          <li><a href="#sobre" onClick={() => setMenuOpen(false)}>Sobre</a></li>
+          <li><a href="#skills" onClick={() => setMenuOpen(false)}>Skills</a></li>
+          <li><a href="#projetos" onClick={() => setMenuOpen(false)}>Projetos</a></li>
+          <li><a href="#contato" onClick={() => setMenuOpen(false)}>Contato</a></li>
         </ul>
-      </nav>
-  </S.NavContainer>
-  )
-  
+      </S.NavMenu>
+    </S.NavContainer>
+  );
 }
 
 export default Nav;
--- src/components/Nav/styles.js
+++ src/components/Nav/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const NavContainer = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
   margin: 35px auto;
   font-family: 'Poppins', sans-serif;
@@ -10,51 +10,85 @@ export const NavContainer = styled.div`
   height: 90px;
   border: 3px solid #0095E5;
   border-radius: 75px;
+  padding: 0 20px;
+  
+  img {
+    height: 35px;
+    width: auto;
 
-  h1 {
-    color: #0095E5;
-    font-size: 2rem;
-    text-align: center;
-    margin-top: 20px;
+    @media (max-width: 425px) {
+      height: 25px; /* Reduz a logo no mobile */
+    }
   }
+`;
 
-    nav ul {
-    list-style: none; /* Remove os marcadores das listas */
-    padding: 0; /* Remove o padding padrão */
-    margin: 0; /* Remove a margem padrão */
-    display: flex; /* Exibe os itens em linha */
+// Botão do Menu Hamburguer
+export const MenuButton = styled.button`
+  display: none;
+  font-size: 2rem;
+  background: none;
+  border: none;
+  cursor: pointer;
+  color: #0095E5;
+
+  @media (max-width: 425px) {
+    display: block;
+  }
+`;
+
+// Menu Responsivo
+export const NavMenu = styled.nav`
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
     
+    @media (max-width: 425px) {
+      flex-direction: column;
+      position: absolute;
+      top: 90px;
+      left: 0;
+      width: 100%;
+      background: white;
+      box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+      padding: 20px;
+      transform: ${({ open }) => (open ? "translateX(0)" : "translateX(-100%)")};
+      transition: transform 0.3s ease-in-out;
+    }
   }
 
-  nav ul li {
-    margin-right: 20px; /* Adiciona espaço entre os itens */
+  li {
+    margin-right: 20px;
     font-size: 1.5rem;
     
+    @media (max-width: 425px) {
+      margin: 15px 0;
+      text-align: center;
+    }
   }
 
-  nav ul li a {
-    text-decoration: none; /* Remove o sublinhado dos links */
-    color: inherit; /* Herda a cor do texto */
-    position: relative; /* Necessário para o ::after */
+  a {
+    text-decoration: none;
+    color: inherit;
+    position: relative;
 
     &::after {
       content: '';
       position: absolute;
-      width: 0px;
+      width: 0;
       height: 2px;
       display: block;
       margin-top: 1px;
       right: 0;
       background: #0095E5;
       transition: width 0.3s ease;
-      -webkit-transition: width 0.3s ease;
     }
 
     &:hover::after {
-      width: 100%; /* A linha aparece ao passar o mouse */
+      width: 100%;
       left: 0;
       background: #0095E5;
     }
-  
   }
 `;
