36db144cfd3afbacffea4a4ee4a8c804e393c5a0
Bruno Chaves <brunochavesuk@icloud.com>
2025-03-20 21:30:32 -0300

    Organizei e finalizei o componente Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,23 @@
 import * as S from './styles';
+import logo from '../../assets/brunodev.svg';
 
 function Nav() {
   return (
     <S.NavContainer>
-   <h1>Olá Mundo!</h1>
-  
+   
+   <a href="#inicio">
+        <img src={logo} alt="Logo Bruno Dev" />
+      </a>
+   
+   <nav>
+        <ul>
+          <li><a href="#inicio">Início</a></li>
+          <li><a href="#sobre">Sobre</a></li>
+          <li><a href="#skills">Skills</a></li>
+          <li><a href="#projetos">Projetos</a></li>
+          <li><a href="#contato">Contato</a></li>
+        </ul>
+      </nav>
   </S.NavContainer>
   )
   
--- src/components/Nav/styles.js
+++ src/components/Nav/styles.js
@@ -1,12 +1,60 @@
 import styled from "styled-components";
 
 export const NavContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin: 35px auto;
   font-family: 'Poppins', sans-serif;
+  width: 80%;
+  height: 90px;
+  border: 3px solid #0095E5;
+  border-radius: 75px;
 
   h1 {
     color: #0095E5;
     font-size: 2rem;
     text-align: center;
     margin-top: 20px;
+  }
+
+    nav ul {
+    list-style: none; /* Remove os marcadores das listas */
+    padding: 0; /* Remove o padding padrão */
+    margin: 0; /* Remove a margem padrão */
+    display: flex; /* Exibe os itens em linha */
+    
+  }
+
+  nav ul li {
+    margin-right: 20px; /* Adiciona espaço entre os itens */
+    font-size: 1.5rem;
+    
+  }
+
+  nav ul li a {
+    text-decoration: none; /* Remove o sublinhado dos links */
+    color: inherit; /* Herda a cor do texto */
+    position: relative; /* Necessário para o ::after */
+
+    &::after {
+      content: '';
+      position: absolute;
+      width: 0px;
+      height: 2px;
+      display: block;
+      margin-top: 1px;
+      right: 0;
+      background: #0095E5;
+      transition: width 0.3s ease;
+      -webkit-transition: width 0.3s ease;
+    }
+
+    &:hover::after {
+      width: 100%; /* A linha aparece ao passar o mouse */
+      left: 0;
+      background: #0095E5;
+    }
+  
   }
 `;
